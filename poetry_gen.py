5d8c3120d7540bfe54d989a158c14f90ac173a3d
Emre Andican <eandican@dhcp-202-197.student-wlan.bowdoin.edu>
2024-11-19 15:48:56 -0500

    fix

---
+++ poetry_gen.py
@@ -0,0 +1,66 @@
+import textstat
+
+
+'''
+A limerick is a five-line poem with a strict rhyme scheme and rhythm: 
+The first, second, and fifth lines rhyme with each other. 
+The third and fourth lines rhyme with each other. 
+The first, second, and fifth lines have three anapests, while the third and fourth lines have two anapests. 
+    An anapest is a rhythm where two unstressed syllables are followed by a stressed syllable
+'''
+
+
+class InspiringSet:
+    def __init__(self):
+        self.inspiring_poems = []
+
+    def generate_inspiring_poems(self, authors_poems):
+        for poem in authors_poems:
+
+    
+    def generate_words(self):
+        'find all words'
+
+    def generate_adjectives(self):
+        'find all adjectives'
+
+    def generate_verbs(self:)
+        'find all verbs'
+
+
+class Poem:
+    def __init__(self, name, lines, flesch_score):
+        name = ""
+        lines = []
+        flesch_score = 0
+
+    def generate_poem(self, inspring_set):
+        'empty line array'
+        'from list of inspiring poems'
+            'Start with noun'
+                'add noun to empty sentence'
+                    'use spacey 
+                    'create lines based on constraints of limerick then add'
+                    'make sure of rhyme types'
+        'name = first line'
+        'create Poem'
+
+    def evaluate_poem(self, poem):
+        'correct poem grammar using gingerit'
+        'score poem textstat.flesch_reading_ease(sentence) or Flesch Reading Ease Score = 206.835 − 1.015 × ( Total Words / Total Sentences ) − 84.6 × ( Total Syllables / Total Words )'
+        'add to poem'
+
+class Teacher:
+    'tts thing'
+
+def main():
+    'empty array for poems'
+    'for loop'
+        'create poem'
+            'generate'
+            'eval'
+    'maybe use pandas to create clusters, choose one closest to 5'
+        'have tts read it out'
+
+
+    
